c9a01e918cdcd38b27bba15cf84b88be05d67eb6
Sepideh <sepidehfallah2@gmail.com>
2020-01-22 11:49:15 -0700

    App setup, Navigation with react router

---
+++ client/src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList}></Route>
+          <Route path="/streams/new" exact component={StreamCreate}></Route>
+          <Route path="/streams/edit" exact component={StreamEdit}></Route>
+          <Route path="/streams/delete" exact component={StreamDelete}></Route>
+          <Route path="/streams/show" exact component={StreamShow}></Route>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
