60ef7e3e611916de5de5d5886b4ed2622a9a54b7
Sepideh <sepidehfallah2@gmail.com>
2020-02-12 12:26:21 -0700

    Making modal Reusable, Using react.fragment tag

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -2,10 +2,21 @@ import React from "react";
 import Modal from "../Modal";
 
 const StreamDelete = () => {
+  const actions = (
+    <>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  );
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Strem"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
     </div>
   );
 };
