cbd43a0e28b4c7f439c2c091a82fadc9c9504b5c
Sepideh <sepidehfallah2@gmail.com>
2020-02-12 12:12:08 -0700

    Hiding modal by calling the history object and handling propagation

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDom from "react-dom";
+import history from "../history";
 
 const Modal = props => {
   return ReactDom.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standared modal visible active">
-        jasdkfjksdjfksdhjsdfhjkfh
+    <div
+      onClick={() => history.push("/")}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={e => e.stopPropagation()}
+        className="ui standared modal visible active"
+      >
+        <div className="header">Delete Strem</div>
+        <div className="content">
+          Are you sure you want to delete this stream?
+        </div>
+        <div className="actions">
+          <button className="ui primary button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
       </div>
     </div>,
     document.querySelector("#modal")
